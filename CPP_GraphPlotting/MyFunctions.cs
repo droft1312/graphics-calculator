840044b31a449646a43f7f47c9e05120e66c4db3
Turar Jumaniyaz <droft1312@gmail.com>
2019-01-25 19:51:25 +0100

    created new static class and added nuget package ValueTuple

---
+++ CPP_GraphPlotting/MyFunctions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPP_GraphPlotting
+{
+    static class MyFunctions
+    {
+        public static Tuple<int, int> GetNewRangeBasedUponOldOne(BaseNode function, double lowerBoundary, double upperBoundary) {
+            return Tuple.Create<int, int> (0, 0);
+        }
+    }
+}
