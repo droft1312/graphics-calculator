1b621b7e4a791002d0c813b197f16c76786ae8ec
Turar Jumaniyaz <droft1312@gmail.com>
2019-01-26 13:06:19 +0100

    unittests project created

---
+++ GraphPlotting_UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("GraphPlotting_UnitTests")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("GraphPlotting_UnitTests")]
+[assembly: AssemblyCopyright ("Copyright ©  2019")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+[assembly: ComVisible (false)]
+
+[assembly: Guid ("8fd9e9cd-ef31-4799-8ac5-564077061c7b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
