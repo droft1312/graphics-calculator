cd2e9861c73d85b3a30812d1399bf5b5295dd5b9
Turar Jumaniyaz <droft1312@gmail.com>
2019-01-26 19:25:05 +0100

    created a test for lagrange and added reference to oxyplot for testsproject

---
+++ GraphPlotting_UnitTests/LagrangeTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CPP_GraphPlotting;
+using OxyPlot;
+
+namespace GraphPlotting_UnitTests
+{
+    /// <summary>
+    /// Summary description for LagrangeTests
+    /// </summary>
+    [TestClass]
+    public class LagrangeTests
+    {
+        [TestMethod]
+        public void ProduceLagrange_Test() {
+            var plotter = new Plotter ();
+            DataPoint[] dataPoints = new DataPoint[3];
+            dataPoints[0] = new DataPoint (0, 15);
+            dataPoints[1] = new DataPoint (10, 3);
+            dataPoints[2] = new DataPoint (20, 30);
+
+            var function = plotter.CreatePolynomialThroughPoints (dataPoints);
+
+            Assert.AreEqual (15d, function.Calculate (0), 0);
+        }
+    }
+}
