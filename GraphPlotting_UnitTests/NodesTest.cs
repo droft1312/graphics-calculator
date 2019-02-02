5ba1e85a7ef9c200db9a0f84455f9b3b7c77d6f3
Turar Jumaniyaz <droft1312@gmail.com>
2019-02-02 19:15:46 +0100

    new tests added

--- GraphPlotting_UnitTests/NodesTest.cs
+++ GraphPlotting_UnitTests/NodesTest.cs
@@ -22,8 +22,45 @@ namespace GraphPlotting_UnitTests
             Assert.AreEqual ("x", func.ToString ());
         }
 
-        public void DivisionNode_CreateDerivativeTree_Test() {
+        [TestMethod]
+        public void DivisionNodeSimplify_Test() {
+            var plotter = new Plotter ();
+            plotter.ProcessString ("/(10,5)");
+            var simplified = plotter.SimplifyTree (plotter.Root);
 
+            Assert.AreEqual (plotter.ProcessTree (0, plotter.Root), plotter.ProcessTree (0, simplified));
+        }
+
+        [TestMethod]
+        public void ExponentialNode_Test() {
+            var plotter = new Plotter ();
+            plotter.ProcessString ("/(s(x),e(x))");
+
+            var expected = -6.71884969743d;
+            var result = plotter.ProcessTree (-2, plotter.Root);
+
+            Assert.AreEqual (expected, result, 3);
+        }
+
+        [TestMethod]
+        public void ExponentialNodeDerivative_Test() {
+            var plotter = new Plotter ();
+            plotter.ProcessString ("/(s(x),e(x))");
+            plotter.CreateDerivativeTree ();
+
+            var expected = 3.64392d;
+            var result = plotter.ProcessTree (-2, Plotter.derivativeRoot);
+
+            Assert.AreEqual (expected, result, 3);
+        }
+
+        [TestMethod]
+        public void FactorialNodeDerivative_Test() {
+            var plotter = new Plotter ();
+            plotter.ProcessString ("!(5)");
+            plotter.CreateDerivativeTree ();
+
+            Assert.AreEqual (0, plotter.ProcessTree (0, Plotter.derivativeRoot));
         }
     }
 }
